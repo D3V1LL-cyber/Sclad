c6d80a7b8f7c06c59e5caf767db02243c407dd67
Vladimir420911 <dhbtgvhb@gmail.com>
2025-02-14 09:06:43 +0300

    добавил БД

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<DbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Sclad")));
 
 var app = builder.Build();
 
