ec8603e84756b92affcf5a1da3e062b27c899a66
Vladimir420911 <dhbtgvhb@gmail.com>
2025-02-27 09:35:20 +0300

    исправил подключение

--- Models/Sclad.cs
+++ Models/Sclad.cs
@@ -4,12 +4,6 @@ namespace Склад.Models
 {
     public class Sclad: DbContext
     {
-        public Sclad(DbContextOptions<Sclad> options) : base(options) { }
-
-        public DbSet<Product> Products { get; set; }
-
-        public DbSet<Category> Categories { get; set; }
-
-        public DbSet<Supplier> Suppliers { get; set; }
+        
     }
 }
