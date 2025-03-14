5a598fa998d44faf74cc0a48f377bd2c3e59f767
Vladimir420911 <dhbtgvhb@gmail.com>
2025-03-14 08:27:11 +0300

    Поменял ApplicationDatabaseContext

--- Data/ApplicationDatabaseContext.cs
+++ Data/ApplicationDatabaseContext.cs
@@ -1,8 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Склад.Models;
 
 namespace Склад.Data
 {
     public class ApplicationDatabaseContext: DbContext
     {
+        public ApplicationDatabaseContext(DbContextOptions<ApplicationDatabaseContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Supplier> Suppliers { get; set; }
     }
 }
--- Models/Sclad.cs
+++ Models/Sclad.cs
@@ -2,14 +2,7 @@
 
 namespace Склад.Models
 {
-    public class Sclad: DbContext
+    public class Sclad
     {
-        public Sclad(DbContextOptions<Sclad> options) : base(options) { }
-
-        public DbSet<Product> Products { get; set; }
-
-        public DbSet<Category> Categories { get; set; }
-
-        public DbSet<Supplier> Suppliers { get; set; }
     }
 }
