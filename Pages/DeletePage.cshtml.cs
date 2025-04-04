c9283cdc94586425f9b37410305d388f74d880fb
Vladimir420911 <dhbtgvhb@gmail.com>
2025-04-04 08:58:47 +0300

    Добавил пустую страницу для удаления

---
+++ Pages/DeletePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Склад.Pages
+{
+    public class DeletePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
