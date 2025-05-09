8ef80c547d5a3603bc1e4683d5ae9392fff7a457
Vladimir420911 <dhbtgvhb@gmail.com>
2025-05-09 13:26:14 +0300

    аааааа

---
+++ UnitTests/ProductTests.cs
@@ -0,0 +1,32 @@
+﻿using Склад.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestProject
+{
+    [TestClass]
+    public sealed class ProductTests
+    {
+        [TestMethod]
+        public void Product_EmptyTitle_ShouldBeInvalid()
+        {
+            // Arrange
+            var product = new Product
+            {
+                Name = "",
+                Price = 100.00m,
+                Quantity = 50,
+                CategoryId = 1,
+                SupplierId = 1
+            };
+
+            // Act
+            var context = new ValidationContext(product);
+            var results = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(product, context, results, true);
+
+            // Assert
+            Assert.IsFalse(isValid);
+            Assert.IsTrue(results.Any(r => r.ErrorMessage == "Введите название товара"));
+        }
+    }
+}
