661e33a0e49fbf4c5f720006c48d23ce9fb42e17
Vladimir420911 <dhbtgvhb@gmail.com>
2025-05-18 18:25:39 +0300

    Изменил home page

--- UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee8e05170c506ce96f8983bda4db93c33308875e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+502e84c94414062483a00813789c1544e004e917")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
